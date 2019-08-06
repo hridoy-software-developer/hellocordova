bd64182e3016c5ed54e90fafa266dc7a59916c4c
hridoy-softeare-develoer <hridoy.prochitoits@gmail.com>
2019-08-06 09:03:53 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// PHP SERVER CREATION /////
+const PHPServer = require('php-server-manager');
+
+const server = new PHPServer({
+    port: 5555,
+    directory: __dirname,
+    directives: {
+        display_errors: 1,
+        expose_php: 1
+    }
+});
+
+server.run();
+
+var opn = require('opn');
+
+// opens the url in the default browser 
+opn('http://'+server.host+':'+server.port+'/www/index.php');
